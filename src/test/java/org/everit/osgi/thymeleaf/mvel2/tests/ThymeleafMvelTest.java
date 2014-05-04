da63c29e43dbc9afc2faf9b448748ce69ad97a4d
balazs.zsoldos <balazs.zsoldos@everit.biz>
2014-05-04 21:02:17 +0200

    Adding license headers

--- src/test/java/org/everit/osgi/thymeleaf/mvel2/tests/ThymeleafMvelTest.java
+++ src/test/java/org/everit/osgi/thymeleaf/mvel2/tests/ThymeleafMvelTest.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Everit - Thymeleaf MVEL2 Extension.
+ *
+ * Everit - Thymeleaf MVEL2 Extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Everit - Thymeleaf MVEL2 Extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit - Thymeleaf MVEL2 Extension.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.everit.osgi.thymeleaf.mvel2.tests;
 
 import java.util.ArrayList;
